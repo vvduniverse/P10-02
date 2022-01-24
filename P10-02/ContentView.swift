3fc66f7b1c026fd67e89e2de2401090a646d7ecb
Vahtee Boo <vahtee.boo@gmail.com>
2022-01-24 12:49:13 +0200

    Part two done

--- P10-02/ContentView.swift
+++ P10-02/ContentView.swift
@@ -7,10 +7,48 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
 struct ContentView: View {
+    @State private var result = [Result]()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(result, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+            }
+        }
+        .task {
+            await loadData()
+        }
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                result = decodedResponse.results
+            }
+        } catch {
+            print("Invalid data")
+        }
+        
     }
 }
 
